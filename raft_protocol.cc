f1d681a966652cba5f094debb6c57c93170e3d12
QianBowen <925763182@qq.com>
2021-11-23 09:32:57 +0800

    finish part1

--- raft_protocol.cc
+++ raft_protocol.cc
@@ -2,36 +2,46 @@
 
 marshall& operator<<(marshall &m, const request_vote_args& args) {
     // Your code here
-
+    m<<args.term;
+    m<<args.candidate_id;
+    m<<args.last_log_index;
+    m<<args.last_log_term;
     return m;
 
 }
 unmarshall& operator>>(unmarshall &u, request_vote_args& args) {
     // Your code here
-
+    u>>args.term;
+    u>>args.candidate_id;
+    u>>args.last_log_index;
+    u>>args.last_log_term;
     return u;
 }
 
 marshall& operator<<(marshall &m, const request_vote_reply& reply) {
     // Your code here
-
+    m<<reply.term;
+    m<<reply.vote_granted;
     return m;
 }
 
 unmarshall& operator>>(unmarshall &u, request_vote_reply& reply) {
     // Your code here
-
+    u>>reply.term;
+    u>>reply.vote_granted;
     return u;
 }
 
 marshall& operator<<(marshall &m, const append_entries_reply& reply) {
     // Your code here
-
+    m<<reply.term;
+    m<<reply.success;
     return m;
 }
 unmarshall& operator>>(unmarshall &m, append_entries_reply& reply) {
     // Your code here
-
+    m>>reply.term;
+    m>>reply.success;
     return m;
 }
 
