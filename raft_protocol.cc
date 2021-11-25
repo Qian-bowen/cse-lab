52c05795987a8744923ba82bb4a45552cd855699
QianBowen <925763182@qq.com>
2021-11-25 08:17:03 +0800

    finish part2

--- raft_protocol.cc
+++ raft_protocol.cc
@@ -35,12 +35,14 @@ unmarshall& operator>>(unmarshall &u, request_vote_reply& reply) {
 marshall& operator<<(marshall &m, const append_entries_reply& reply) {
     // Your code here
     m<<reply.term;
+    m<<reply.high_log_entry_index;
     m<<reply.success;
     return m;
 }
 unmarshall& operator>>(unmarshall &m, append_entries_reply& reply) {
     // Your code here
     m>>reply.term;
+    m>>reply.high_log_entry_index;
     m>>reply.success;
     return m;
 }
--- raft_test_utils.h
+++ raft_test_utils.h
@@ -331,6 +331,8 @@ int raft_group<state_machine, command>::append_new_command(int value, int expect
     list_command cmd(value);
     auto start = std::chrono::system_clock::now();
     int leader_idx = 0;
+    int test_value=999;//just for test
+    int commit_max=INT32_MIN;
     while (std::chrono::system_clock::now() < start + std::chrono::seconds(10)) {
         int log_idx = -1;
         for (size_t i = 0; i < nodes.size(); i++) {
@@ -342,6 +344,7 @@ int raft_group<state_machine, command>::append_new_command(int value, int expect
             bool is_leader = nodes[leader_idx]->new_command(cmd, temp_term, temp_idx);
             if (is_leader) {
                 log_idx = temp_idx;
+                // std::cerr<<"log_idx:"<<log_idx<<std::endl;
                 break;
             }
         }
@@ -350,9 +353,15 @@ int raft_group<state_machine, command>::append_new_command(int value, int expect
             while (std::chrono::system_clock::now() < check_start + std::chrono::seconds(2)) {
                 int committed_server = num_committed(log_idx);
                 // std::cout << "nun commited: " << committed_server << std::endl;
+
+                commit_max=std::max(commit_max,committed_server);//test
+
                 if (committed_server >= expected_servers) {
                     // The log is committed!
                     int commited_value = get_committed_value(log_idx);
+
+                    test_value=commited_value;//test
+
                     if (commited_value == value)
                         return log_idx; // and the log is what we want!
                 }
@@ -363,7 +372,8 @@ int raft_group<state_machine, command>::append_new_command(int value, int expect
             mssleep(50);
         }
     }
-    ASSERT(0, "Cannot make agreement!");
+    // ASSERT(0, "Cannot make agreement!");
+    ASSERT(0, "Cannot make agreement!"<< "value:" << value << " "<<"commit value:" << test_value << " commit max:"<<commit_max);
     return -1;
 }
 
