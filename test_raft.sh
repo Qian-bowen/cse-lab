117df53ab9ce8080c03db0a40fbeccaef389e0d8
QianBowen <925763182@qq.com>
2021-12-19 18:52:02 +0800

    lab3 snapshot todo, do lab4 first

--- test_raft.sh
+++ test_raft.sh
@@ -1,12 +1,13 @@
-END=100
-## print date five times ##
+END=10
 x=$END 
-echo "" > output1.txt
+echo "" > output.txt
 while [ $x -gt 0 ]; 
 do 
     echo "test";
-    echo "start test part2" >> output1.txt;
-    ./raft_test part2 backup >> output1.txt;
+    echo "start test part3" >> output.txt;
+    ./raft_test part2 >> output.txt;
+    # echo "start test part2" >> output.txt;
+    # ./raft_test part2 >> output.txt;
     # echo "start test part1" >> output.txt;
     # ./raft_test part1 >> output.txt;
     x=$(($x-1));
